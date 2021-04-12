88aeafc9c00609ef9aa6238e358c0cdd5842cca8
Akshat10101998 <akshatmangal1010@gmail.com>
2021-04-13 00:59:11 +0530

    first_commit

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 from scholarly import scholarly
 import time
 
-ids = ['x8qCMhcAAAAJ', 'WjHjbrwAAAAJ',
- 'LVfqLuoAAAAJ',
- 'K-AIPzQAAAAJ',
- 'rOum2XsAAAAJ',
- 'n8xuCVkAAAAJ']
+ids = ['K-AIPzQAAAAJ','rOum2XsAAAAJ','n8xuCVkAAAAJ','mF-F-zkAAAAJ',	'chWbG70AAAAJ', '7HJzGQYAAAAJ']
 
 
 for i in ids:
-    author = scholarly.search_author_id(i)
-    print(author['interests'])
-    scholarly.pprint(author)
-    time.sleep(2)
+    try:
+        f = open('resutls.txt','a')
+        author = scholarly.search_author_id(i)
+        # print(author['interests'])
+        # scholarly.pprint(author)
+        print(type(author))
+        f.write(str(author))
+        f.write(',\n')
+        f.close()
+    except:
+        continue
 
 """search_query = scholarly.search_author('A Min Tjoa')
 author = scholarly.fill(next(search_query))
